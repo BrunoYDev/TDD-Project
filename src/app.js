7fa01051788a4572fe7d36aa2573ff99dd464667
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-09-19 15:49:04 -0300

    Add: Create User endpoint

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
 let express = require("express");
 let app = express();
 let mongoose = require("mongoose");
+let user = require("../models/User");
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/imagesharer");
+mongoose.connect("mongodb://localhost:27017/imagesharer").catch(err => console.error(err));
+
+let User = mongoose.model("User",user);
 
 app.get("/", (req,res)=>{
 
     res.status(200).json({message: "Ok"});
+});
+
+app.post("/user", async (req,res) => {
+
+    try {
+        let newUser = new User({name: req.body.name, email: req.body.email, password: req.body.password});
+        await newUser.save();
+        res.status(200).json({email: req.body.email});
+    } catch (error) {
+        res.status(500);
+        console.error(error);
+    }
 });
 
 module.exports = app;
